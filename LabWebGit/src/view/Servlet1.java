d33eb7163c25f9c32831120be71532495afaf827
何旻虹 <hmh@gmail.com>
2019-05-27 14:31:42 +0800

    servlet1 add print

--- LabWebGit/src/view/Servlet1.java
+++ LabWebGit/src/view/Servlet1.java
@@ -21,6 +21,8 @@ public class Servlet1 extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		System.out.print("b");
 		this.doGet(request, response);
+		
 	}
 }
